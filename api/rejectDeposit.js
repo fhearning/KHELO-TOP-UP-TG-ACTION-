cd54f93c67a714dea7c6e6756b7fe09e5ee4eff5
fhearning <mrsjesminshara@gmail.com>
2026-03-16 21:55:47 +0600

    Update rejectDeposit.js

--- api/rejectDeposit.js
+++ api/rejectDeposit.js
@@ -1,13 +1,11 @@
 import {db} from "../firebase.js"
 
-export default async function handler(req,res){
+export default async function rejectDeposit(data){
 
-const id=req.query.id
+const id=data.replace("reject_deposit_","")
 
 await db.collection("deposits").doc(id).update({
 status:"rejected"
 })
-
-res.json({success:true})
 
 }
